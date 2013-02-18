0e0e7a78095678caa11a493d40bab32817e94a34
Mike Gallagher <mikesligo@gmail.com>
2013-02-18 21:58:18 +0000

    doesn't work but cleaned up

--- pie.py
+++ pie.py
@@ -2,6 +2,7 @@
 
 import re
 import socket               # Import socket module
+from threading import Thread
 from struct import *
 
 class Packet:
@@ -10,12 +11,8 @@ class Packet:
         print packet
         self.full = packet + "\r\n"
         if "GET" in packet[0:3]:
-            host = re.search('Host: (\w+).(\w+).(\w+)',packet)
-            host = host.group(1) +'.'+ host.group(2) +'.'+ host.group(3)
-            if host is None:
-                host = re.search('Host: (\w+).(\w+)',packet)
-                host = host.group(1) +'.'+ host.group(2)
-            self.host = host
+            host = re.search('Host: (\S+)',packet)
+            self.host = host.group(1)
 
     def printpacket(self):
         print 'All: \n' + self.full
@@ -24,40 +21,55 @@ class Server:
     def __init__(self, host, port):
         self.port = port;
         self.host = host;
-        self.s = socket.socket()
-        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # don't bind address
-        self.s.bind((host,port))
-        self.s.listen(5)
+        self.mainsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.mainsocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # don't bind address
+        self.mainsocket.bind((host,port))
+        self.mainsocket.listen(5)
 
-    def receive(self):
-        conn, addr = self.s.accept()     # Establish connection with client.
+    def start(self):
+        print "Starting PieProxy..."
+        self.conn, addr = self.mainsocket.accept()     # Establish connection with client.
+        listen = Thread(target=self.listen_for_incoming_client,args=())
+        listen.start()
 
-        packet = conn.recvfrom(8192)
-        packet = Packet(packet)
-        response = self.forward_packet(packet)
-        conn.sendall(response.full)
+    def listen_for_incoming_client(self): # To be run in a thread
+        while True:
+            print "Listening for incoming client..."
+            packet = self.conn.recvfrom(8192)
+            packet = Packet(packet)
+            self.forward_packet_to_server(packet)
+            raw_input("\nHit enter to continue")
 
-        conn.close()
+    def forward_packet_to_server(self, packet):
+        print "Forwarding packet to server..."
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            print 'Connecting to '+packet.host
+            s.connect((packet.host,80))
+            s.sendall(packet.full)
+        except:
+            print "ERROR ATTEMPTING TO CONNET OR SEND PACKETS\n============================\n"
+            print packet.full
+        self.listen_for_incoming_server(s)
 
-    def forward_packet(self, packet):
-        s = socket.socket()
-        print 'Connecting to '+packet.host
-        s.connect((packet.host,80))
-        s.sendall(packet.full)
-        response = s.recvfrom(8192)
+    def listen_for_incoming_server(self,socket):
+        print "Listening for incoming packets from the server"
+        response = socket.recvfrom(8192)
         response = Packet(response)
-        s.close()
-        return response
+        self.return_response_to_client(response)
+        socket.close()
 
-    def send_socket(self,socket):
-        socket.send('Thank you for connecting')
+    def return_response_to_client(self, response):
+        print "Returning response to client..."
+        self.conn.sendall(response.full)
 
     def close(self):
-        self.s.close()
+        self.mainsocket.close()
 
 if __name__ == '__main__':
+    print
     print "Pie Proxy\n=============================\n"
     host = socket.gethostname() # Get local machine name
     port = 8000                # Reserve port
     server = Server(host,port)
-    server.receive()
+    server.start()
